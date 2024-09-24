bdc3935f1f36bd8f9922b15bb469309c5f87c9b6
Emil A. Siemes <emil@Emils-MacBook-Pro.fritz.box>
2024-09-24 16:34:03 +0200

    configs for grafana in k8s

--- k8s/install-google-cloud-shell-minikube.sh
+++ k8s/install-google-cloud-shell-minikube.sh
@@ -4,8 +4,8 @@ export GRAFANA_PUBLICPORT=3333
 echo WEB_HOST: $WEB_HOST
 
 echo Installing nginx. Access DB console on port 8080. Grafana on port $GRAFANA_PUBLICPORT
-export 
-envsubst < ../nginx.conf.template >../nginx.conf
+
+envsubst '${WEB_HOST},${GRAFANA_PUBLICPORT}' < ../nginx.conf.template >../nginx.conf
 sudo apt-get update; sudo apt-get -y install nginx; sudo sudo cp ../nginx.conf /etc/nginx/nginx.conf; sudo nginx
 
 minikube start --cpus=4 --memory=6G
@@ -63,6 +63,8 @@ kubectl create configmap cloudshell-config --from-file=./grafana.ini  --namespac
 kubectl apply -f grafana.yaml --namespace=monitoring
 
 kubectl port-forward -n monitoring svc/prometheus 9090:9091 &
+
+kubectl wait --for=condition=ready pod -n monitoring -l app=grafana
 kubectl port-forward -n monitoring svc/grafana 3030:3000 &
 
 kubectl port-forward -n cockroachdb svc/cockroachdb 18080:8080 
